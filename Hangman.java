697805942f018980dd7f366d34cfc8b8fa907d46
Aaron Rosenberg <ajrosenberg20@gmail.com>
2025-10-21 13:16:24 -0500

    Game is in working state for non-misinput by user

--- Hangman.java
+++ Hangman.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class Hangman {
 
     static String[][] wordBank = {
-            { "Bane", "Cape", "Cowl", "Croc", "Fear" },
+            /*Length 4*/{ "Bane", "Cape", "Cowl", "Croc", "Fear" },
             { "Alley", "Bruce", "Clown", "Crime", "Joker", "Manor", "Quinn", "Robin", "Wayne", "Zsasz" },
             { "Alfred", "Arkham", "Asylum", "Enigma", "Freeze", "Gordon", "Gotham", "Harley", "Knight", "League",
                     "Lucius", "Oracle", "Signal","Batman" },
             { "Batcave", "Batgirl", "Falcone", "Grapple", "Justice", "Penguin", "Riddler", "Shadows",
                     "Twoface", "Utility" },
             { "Batarang", "Catwoman", "Clayface", "Nightwing" },
-            { "Batmobile", "Detective", "Scarecrow", "Vengeance", "Vigilante" }
+            /*Length 9*/{ "Batmobile", "Detective", "Scarecrow", "Vengeance", "Vigilante" }
     };
 
     static String[] hangmanIterations = { """
@@ -93,9 +93,16 @@ public class Hangman {
     {
        // print out your word
         System.out.print("\nYour Word: ");
-        for (String s : underlineWordArray) {
-            System.out.print(s + " ");
-        } 
+
+        for(int i = 0; i < underlineWordArray.length;i++)
+        {
+            if(i==0 && (!underlineWordArray[i].equals("_")))
+            {
+                System.out.print(underlineWordArray[i].toUpperCase() + " ");            
+            }
+            else 
+                System.out.print(underlineWordArray[i] + " ");  
+         }
     }
 
     //chooses a random word to start the game,
@@ -118,35 +125,35 @@ public class Hangman {
     }
 
     public static String chooseSpecificLengthWordForGame(int letterLength) {
-
-        int index;
+         
+        String returnString = "";
         switch (letterLength) {
             case 4:
-                index = 0;
+                returnString = wordBank[0][(int) (Math.random() * wordBank[0].length - 1)];
                 break;
             case 5:
-                index = 0;
+                returnString = wordBank[1][(int) (Math.random() * wordBank[1].length - 1)];
                 break;
             case 6:
-                index = 0;
+                returnString = wordBank[2][(int) (Math.random() * wordBank[2].length - 1)];
                 break;
             case 7:
-                index = 0;
+                returnString = wordBank[3][(int) (Math.random() * wordBank[3].length - 1)];
                 break;
             case 8:
-                index = 0;
+                returnString = wordBank[4][(int) (Math.random() * wordBank[4].length - 1)];
                 break;
             case 9:
-                index = 0;
+                returnString = wordBank[5][(int) (Math.random() * wordBank[5].length - 1)];
                 break;
 
         }
 
-        return wordBank[letterLength][(int) (Math.random() * wordBank[letterLength].length - 1)];
+        return returnString;
     }
 
     public static boolean playHangman(String word) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
 
         // keeps track of the ascii image index we want to show the user
         int hangmanIterationsIndex = 0;
@@ -168,49 +175,62 @@ public class Hangman {
         printOutWord(underlineWordArray);
 
         //String that holds the guess for each round
-        String guessString = "";
+         String guessString = "";
 
-        // guess a letter until win condition or lose condition
-        while(hangmanIterationsIndex < hangmanIterations.length) {
+        // // guess a letter until win lose condition
+         boolean finishedWord = false;
+         while(hangmanIterationsIndex < hangmanIterations.length-1 && finishedWord == false) {
             System.out.print("\nWhat letter would you like to guess: ");
-            guessString = scan.nextLine();
+            guessString = scan2.nextLine();
             
-            //this string is used so that we can keep the original case format of guess string
-            String lowerCaseGuessString = guessString.toLowerCase();
+           //this string is used so that we can keep the original case format of guess string
+             String lowerCaseGuessString = guessString.toLowerCase();
 
-            if (word.toLowerCase().contains(lowerCaseGuessString)) {
-                System.out.println("\nThe letter " + guessString + " is in the word. Good job.");
-                System.out.println(hangmanIterations[hangmanIterationsIndex]);
+             if (word.toLowerCase().contains(lowerCaseGuessString)) {
+                 System.out.println("\nThe letter " + guessString + " is in the word. Good job.");
+                 System.out.println(hangmanIterations[hangmanIterationsIndex]);
 
-                //Adds the guessed letter anywhere it appeared in the word
-                for (int i = 0; i < wordArray.length; i++) {
-                    if (wordArray[i].toLowerCase().equals(lowerCaseGuessString)) {
-                        underlineWordArray[i] = guessString;
-                    }
-                }
+                 //Adds the guessed letter anywhere it appeared in the word
+                 for (int i = 0; i < wordArray.length; i++) {
+                     if (wordArray[i].toLowerCase().equals(lowerCaseGuessString)) {
+                         underlineWordArray[i] = guessString;
+                     }
+                 }
+                  printOutWord(underlineWordArray);
 
-                 printOutWord(underlineWordArray);
+             } else {
+                 hangmanIterationsIndex++;
+                 System.out.println("\nThe letter " + guessString + " is not in the word. So sorry.");
 
-            } else {
-                hangmanIterationsIndex++;
-                System.out.println("\nThe letter " + guessString + " is not in the word. So sorry.");
+                 //only print out the next ascii image if the index is less than the array length
+                 if(hangmanIterationsIndex < hangmanIterations.length){
+                     System.out.println(hangmanIterations[hangmanIterationsIndex]);
+                     printOutWord(underlineWordArray);
+                 }   
+             }
 
-                //only print out the next ascii image if the index is less than the array length
-                if(hangmanIterationsIndex < hangmanIterations.length){
-                    System.out.println(hangmanIterations[hangmanIterationsIndex]);
-                    printOutWord(underlineWordArray);
-                }
+             //check if the user finished word, and if they did set boolean flag to false
+             int wordArrayLength = wordArray.length;
+             int wordArrayLengthCheck = 0;
+             for(int i = 0;i< wordArray.length;i++)
+             {
 
-                 
-            }
-        }
-          
-        scan.close();
+                 if(wordArray[i].toLowerCase().equals(underlineWordArray[i].toLowerCase()))
+                  wordArrayLengthCheck++;
+             }
 
-         if(hangmanIterationsIndex < hangmanIterations.length)
-            return true;
-        else 
-            return false;
+             if(wordArrayLength == wordArrayLengthCheck)
+             {
+                 finishedWord = true;
+                 break;
+             }
+            
+         }
+
+          if(finishedWord)
+             return true;
+         else 
+             return false;
     }
 
      
@@ -220,7 +240,7 @@ public class Hangman {
         System.out.println("Welcome to the Hangman Game");
         System.out.println("Choose from the options below to start a game (Type the letter of option)");
 
-        //menu();
+        menu();
 
         System.out.print("Enter your choice here: ");
 
@@ -228,51 +248,70 @@ public class Hangman {
         Scanner scan = new Scanner(System.in);
 
         // grab the input from the user and make it lower case
+         
         String whatToDo = scan.nextLine();
-        whatToDo = whatToDo.toLowerCase();
+     
 
         // while the input string wasn't exit, we run the menu (the game) again
         while (!whatToDo.equals("c")) {
 
             String word;
+             
 
-            switch (whatToDo) {
-                case "a":
-                    word = chooseRandomWordForGame();
+            switch (whatToDo.toLowerCase()) {
+                 case "a":
+                     word = chooseRandomWordForGame();
+                      
 
-                    boolean winCondition = playHangman(word);
+                     boolean winCondition = playHangman(word);
+
+                     if(winCondition)
+                     {
+                         System.out.println("\nCongrats, you won!!!" + " The word was '" + word + "'");
+                     }
+                     else
+                     {
+                         System.out.println("\n" + hangmanIterations[hangmanIterations.length-1]);
+                         System.out.println("Tough luck, try again?");
+                     }
+
+                     break;
+
+                 case "b":
+                     System.out.print("\nWhat letter length would you like to play: ");
+                     int letterLength = scan.nextInt();
+                     // call next line again to move out of error caused by nextInt
+                     scan.nextLine();
+
+                     while (letterLength > 9 || letterLength < 4) {
+                         System.out.print("Please choose a length greater than 3 and less than 10: ");
+                         letterLength = scan.nextInt();
+                         scan.nextLine();
+
+                     }
+                     word = chooseSpecificLengthWordForGame(letterLength);
+
+                    winCondition = playHangman(word);
 
                     if(winCondition)
-                        System.out.println("Congrats, you won!!!");
-                    else
-                        System.out.println("Tough luck, try again?");
+                     {
+                         System.out.println("\nCongrats, you won!!!" + " The word was '" + word + "'");
+                     }
+                     else
+                     {
+                         System.out.println("\n" + hangmanIterations[hangmanIterations.length-1]);
+                         System.out.println("Tough luck, try again?");
+                     }
 
-                    break;
+                     break;
+                 default:
 
-                case "b":
-                    System.out.print("\nWhat letter length would you like to play: ");
-                    int letterLength = scan.nextInt();
-                    // call next line again to move out of error caused by nextInt
-                    scan.nextLine();
-
-                    while (letterLength > 12 || letterLength < 4) {
-                        System.out.println("Please choose a length greater than 3 or less than 13: ");
-                        letterLength = scan.nextInt();
-
-                    }
-                    word = chooseSpecificLengthWordForGame(letterLength);
-
-                    playHangman(word);
-
-                    break;
-                default:
-
-                    System.out.println("\nInput: '" + whatToDo + "' is not an option. Please try entering 'A', 'B', or 'C'.");
+                     System.out.println("\n\nInput: '" + whatToDo + "' is not an option. Please try entering 'A', 'B', or 'C'.");
             }
 
-           // menu();
+            menu();
             System.out.print("What would you like to do: ");
-           
+             
             whatToDo = scan.nextLine();
         }
 
